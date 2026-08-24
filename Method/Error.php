5b4112a21c84a9c773c709d00fe4b7566bfc09bd
gizmore <gizmore@wechall.net>
2026-08-24 12:26:04 +0200

    'some error constant missing'

--- Method/Error.php
+++ Method/Error.php
@@ -31,7 +31,7 @@ final class Error extends MethodAjax
 
 	public function execute(): GDT
 	{
-		if (GDO_ERROR_MAIL)
+		if (GDO_ERROR_MAILS)
 		{
 			$url = $this->gdoParameterVar('url');
 			$message = $this->gdoParameterVar('message');
